4ff251be9df23f20a5f367388dd9e89113f27905
Khoa Le <khoatle165@gmail.com>
2019-08-05 15:11:02 +0200

    setup form for word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,20 @@
 class GamesController < ApplicationController
   def new
+    @letters = generate_random_chars(10)
   end
 
   def score
+
+  end
+
+  private
+
+  def generate_random_chars(size)
+    chars = ('A'..'Z').to_a
+    letters = []
+    size.times do
+      letters << chars.sample
+    end
+    letters
   end
 end
